302e0ce579b2a92f68948866dfcd076204506501
Aishworyann <75421318+aishworyann@users.noreply.github.com>
2022-08-09 13:21:25 +0000

    updated

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ public:
  int data;
  Node* next;
 };
+
 //funtion to display the the linked list 
 void display(Node* head){
    while(head){
@@ -15,36 +16,90 @@ void display(Node* head){
 }
 // funtion to insert the node at front 
 void push(Node ** head_ref, int value){
-  
   Node* new_node = new Node(); // allocating the new node
   new_node->data=value;// storing the value to the new node
+  
+  if(*head_ref==NULL){
+    *head_ref=new_node;
+  }else{
   new_node->next= (*head_ref);// pointing the next pointer to current head 
   (*head_ref)=new_node;//making new node as head
+  }
+  
 }
+
+//function to insert the node after some node
+void insertafter(Node *prev_node, int value){
+
+  Node * new_node= new Node();
+  if(prev_node==NULL){
+    cout<<"Previous node is NULL"<<endl;
+  }
+  new_node->data=value;
+  new_node->next=prev_node->next;
+  prev_node->next=new_node;
+}
+
+//function to insert the node at end
+void insert_last(Node **head_ref, int value){
+  
+  Node *new_node = new Node();  // allocate the node
+  Node *last_node= *head_ref;   //make a new node have ref to head node
+  
+  new_node->data=value; 
+  new_node->next=NULL;
+
+  //check if the list is empty or not
+  if((*head_ref)== NULL){
+    *head_ref = new_node; // if empty create the new node
+  }
+    //else traverse the list till the last node 
+  else{
+    while(last_node->next!=NULL){
+      last_node=last_node->next;  //update the next of the last node
+    }
+  }
+
+  last_node->next=new_node; //make the last node pointing to the new node we created
+  
+  
+}
+
 
 int main() {
 
   Node* head=NULL;
-  Node* second=NULL;
-  Node* third=NULL;
-
+  // Node* second=NULL;
+  // Node* third=NULL;
+// Allocate the new nodes
     head = new Node();
-    second = new Node();
-    third = new Node();
+    // second = new Node();
+    // third = new Node();
   
-  head->data=5;
-  head->next=second;
+  // head->data=5;             //inserting the value
+  // head->next=NULL;        //pointing the next pointer to the next node
 
-  second->data=10;
-  second->next=third;
+  // second->data=10;
+  // second->next=third;
 
-  third->data=20;
-  third->next=NULL;
+  // third->data=20;
+  // third->next=NULL;
 
   
 //insertion of node before front 
- push(&head,100); //as we are using double pointer , we need to pass the address of the head pointer in the function 
+ push(&head,100);                   //as we are using double pointer , we need to pass the address                                       of the head pointer in the function 
+                                   // time complexity is O(1)
+  push(&head,200);
+  push(&head,300);
+  push(&head,400);
+//insertion of node after a node 
+ // insertafter(second, 200);        // first arg is the node we want to add the node after , second                                       is the value
+                                  // time complexity is O(n)
   
+//insertion of a node at last
+  insert_last(&head, 1000);
+  
+//fucntion to display the linked list
  display(head);
   
 
